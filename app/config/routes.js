b1ca3254a32b16b348d60a1a33241d6a70dc9848
luongthomas <luongt@oregonstate.edu>
2016-09-14 12:23:39 -0700

    Used imports for modules

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,15 +1,13 @@
-var React = require('react');
-var ReactRouter = require('react-router');
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var IndexRoute = ReactRouter.IndexRoute;
-var hashHistory = ReactRouter.hashHistory;
+'use strict'
+import React from 'react';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 
 var routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
     </Route>
